2f807bf185161eddcab95ba681aa1a787d46d9e5
ocartaxo <oacartax@gmail.com>
2023-08-24 16:28:05 -0300

    model

---
+++ src/main/kotlin/br/com/kofood/pagamentos/model/Payment.kt
@@ -0,0 +1,62 @@
+package br.com.kofood.pagamentos.model
+
+import br.com.kofood.pagamentos.dto.PaymentUpdate
+import jakarta.persistence.*
+import jakarta.validation.constraints.NotBlank
+import jakarta.validation.constraints.NotNull
+import jakarta.validation.constraints.PositiveOrZero
+import jakarta.validation.constraints.Size
+import java.math.BigDecimal
+import kotlin.math.min
+
+
+@Entity
+@Table(name = "pagamentos")
+data class Payment(
+        @Id
+        @GeneratedValue(strategy = GenerationType.IDENTITY)
+        val id: Long ?= null,
+
+        @NotNull
+        @PositiveOrZero
+        @Column(name = "valor")
+        var value: BigDecimal,
+
+        @NotBlank
+        @Size(max=169)
+        @Column(name = "nome")
+        var name: String,
+
+        @NotBlank
+        @Size(max=19)
+        val number: String,
+
+        @NotBlank
+        @Size(max=7)
+        @Column(name = "expiracao")
+        val expiration: String,
+
+        @NotBlank
+        @Size(min = 3, max = 3)
+        @Column(name = "codigo")
+        var code: String,
+
+        @NotNull
+        @Enumerated(value = EnumType.STRING)
+        val status: Status = Status.CREATED,
+
+        @NotNull
+        @Column(name="pedido_id")
+        val orderId: Long,
+
+        @NotNull
+        @Column(name="metodo_pagamento_id")
+        val paymentMethodId: Long
+
+) {
+        fun update(newInfo: PaymentUpdate) {
+                this.value = newInfo.value ?: this.value
+                this.name = newInfo.name ?: this.name
+                this.code = newInfo.code ?: this.code
+        }
+}
---
+++ src/main/kotlin/br/com/kofood/pagamentos/model/Status.kt
@@ -0,0 +1,5 @@
+package br.com.kofood.pagamentos.model
+
+enum class Status {
+    CREATED, CONFIRMED, CANCELLED
+}
