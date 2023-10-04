463b33f54bab09b5f7302f3df4fd450fb50efb66
ocartaxo <oacartax@gmail.com>
2023-10-04 06:36:06 -0300

    erro de argumento inválido na requisição

--- src/main/kotlin/br/com/kofood/pagamentos/error/ErrorHandler.kt
+++ src/main/kotlin/br/com/kofood/pagamentos/error/ErrorHandler.kt
@@ -5,6 +5,7 @@ import org.springframework.http.HttpStatus
 import org.springframework.http.ResponseEntity
 import org.springframework.web.bind.annotation.ExceptionHandler
 import org.springframework.web.bind.annotation.RestControllerAdvice
+import java.lang.IllegalArgumentException
 import java.sql.SQLException
 import java.time.LocalDateTime
 
@@ -12,10 +13,27 @@ import java.time.LocalDateTime
 class ErrorHandler {
 
     @ExceptionHandler(SQLException::class)
-    fun handlerSQLException(ex: SQLException, request: HttpServletRequest): ResponseEntity<Any> {
+    fun handlerSQLException(
+        ex: SQLException,
+        request: HttpServletRequest
+    ): ResponseEntity<Any> {
         return ResponseEntity.badRequest().body(object {
             val timestamp = LocalDateTime.now()
             val message = "Erro ao acessar o banco de dados!"
+            val error = ex.message
+            val status = HttpStatus.BAD_REQUEST
+            val path = request.contextPath
+        })
+    }
+
+    @ExceptionHandler(IllegalArgumentException::class)
+    fun handleIllegalArgumentException(
+        ex: IllegalArgumentException,
+        request: HttpServletRequest
+    ): ResponseEntity<Any> {
+        return ResponseEntity.badRequest().body(object {
+            val timestamp = LocalDateTime.now()
+            val message = "Erro ao processar a requisição!"
             val error = ex.message
             val status = HttpStatus.BAD_REQUEST
             val path = request.contextPath
