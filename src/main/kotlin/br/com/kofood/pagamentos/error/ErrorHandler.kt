be390ea9b17a733f466de870c9446bc257eacb0c
ocartaxo <oacartax@gmail.com>
2023-10-04 06:31:39 -0300

    mensagem de erro

--- src/main/kotlin/br/com/kofood/pagamentos/error/ErrorHandler.kt
+++ src/main/kotlin/br/com/kofood/pagamentos/error/ErrorHandler.kt
@@ -2,7 +2,6 @@ package br.com.kofood.pagamentos.error
 
 import jakarta.servlet.http.HttpServletRequest
 import org.springframework.http.HttpStatus
-import org.springframework.http.HttpStatusCode
 import org.springframework.http.ResponseEntity
 import org.springframework.web.bind.annotation.ExceptionHandler
 import org.springframework.web.bind.annotation.RestControllerAdvice
@@ -16,11 +15,10 @@ class ErrorHandler {
     fun handlerSQLException(ex: SQLException, request: HttpServletRequest): ResponseEntity<Any> {
         return ResponseEntity.badRequest().body(object {
             val timestamp = LocalDateTime.now()
-            val message = "Erro ao realizar consulta no banco"
+            val message = "Erro ao acessar o banco de dados!"
             val error = ex.message
             val status = HttpStatus.BAD_REQUEST
             val path = request.contextPath
         })
     }
-
 }
